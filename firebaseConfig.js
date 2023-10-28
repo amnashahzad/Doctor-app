1be8bc1e7609ac9311fa9eec39fcdd23cf57a8e8
Amna Shahzad <121329321+amnashahzad@users.noreply.github.com>
2023-10-28 03:21:51 -0700

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBuxIHbVFX6jWi_2MUiPOgr4tBZxac2oSE",
+    authDomain: "doctor-app-f1199.firebaseapp.com",
+    projectId: "doctor-app-f1199",
+    storageBucket: "doctor-app-f1199.appspot.com",
+    messagingSenderId: "508941752788",
+    appId: "1:508941752788:web:29e7434a8355261bd19e41",
+    measurementId: "G-9N6JWF1492"
+  };
